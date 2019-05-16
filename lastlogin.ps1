54394cacc11a12fccc58db3adbadb935b518c65e
TomSanterre <45238343+TomSanterre@users.noreply.github.com>
2019-05-16 11:48:11 +0200

    Add files via upload

---
+++ lastlogin.ps1
@@ -0,0 +1,13 @@
+﻿# Affiche la date de dernière connexion
+# d'un compte local système
+$nom=Read-Host -Promt "Saisir un nom de compte local"
+$compte=[ADSI] "WinNT://./$nom"
+if ($compte.path){
+    write-Host $compte.LastLogin
+    Write-Host $compte.Description
+    Write-Host $compte.Fullname
+}
+else {
+    Write-Host "$nom non trouvé"
+    
+}
