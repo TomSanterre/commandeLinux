577ffb9df8609f3c56b723a9b89fc495b586ae85
TomSanterre <45238343+TomSanterre@users.noreply.github.com>
2019-05-16 11:47:32 +0200

    Add files via upload

---
+++ ajoutCompte.ps1
@@ -0,0 +1,19 @@
+﻿# Ajoute un compte dans la base locale du système 
+# à partir de la saisie du nom et de la description
+
+$local=[ADSI]"WinNT://."
+
+$nom=Read-Host -Prompt "Saisir un nom"
+$description=Read-Host -Prompt "Saisir une description"
+
+$compte=[ADSI]"WinNT://./$nom"
+if (!$compte.path){
+    $utilisateur=$local.create("user", $nom)
+    $utilisateur.InvokeSet("FullName", $nomComplet)
+    $utilisateur.InvokeSet("Description", $description)
+    $utilisateur.CommitChanges()
+    Write-Host "$nom ajouté"
+}
+else {
+    Write-Host "$nom éxiste déjà"
+}
